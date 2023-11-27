c344ff228a9b956f404032410ec8a0ad35af3f38
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-11-27 09:01:49 +0800

    lab1; collatz seq

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,10 +2,25 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
+    /** Identifies next number in a Collatz sequence, given N
+     * @param number the nth number
+     * */
+    public static int nextNumber(int number) {
+        boolean isEven = (number % 2 == 0);
+        return  isEven ? (number / 2) : (3 * number + 1);
+    }
+
     public static void main(String[] args) {
         int n = 5;
+
         System.out.print(n + " ");
 
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+
+        System.out.println();
     }
 }
 
